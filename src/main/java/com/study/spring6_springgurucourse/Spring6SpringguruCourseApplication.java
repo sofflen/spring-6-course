ad5b8300378a2c1d8418173b5ce8a321853b75cc
sofflen <nixxesgaming@gmail.com>
2024-08-06 20:22:42 +0300

    initial commit

---
+++ src/main/java/com/study/spring6_springgurucourse/Spring6SpringguruCourseApplication.java
@@ -0,0 +1,13 @@
+package com.study.spring6_springgurucourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6SpringguruCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6SpringguruCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/spring6_springgurucourse/Spring6SpringguruCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.spring6_springgurucourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6SpringguruCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
