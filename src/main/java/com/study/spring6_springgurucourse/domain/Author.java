f4d41b13708c25bd81334444bad871635181f5c1
sofflen <nixxesgaming@gmail.com>
2024-08-07 15:37:48 +0300

    added relationship between Book and Author

--- src/main/java/com/study/spring6_springgurucourse/domain/Author.java
+++ src/main/java/com/study/spring6_springgurucourse/domain/Author.java
@@ -3,6 +3,9 @@ package com.study.spring6_springgurucourse.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+import java.util.Set;
 
 @Entity
 public class Author {
@@ -12,6 +15,8 @@ public class Author {
     private Long id;
     private String firstName;
     private String lastName;
+    @ManyToMany
+    private Set<Book> books;
 
     public Long getId() {
         return id;
@@ -35,5 +40,13 @@ public class Author {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
     }
 }
--- src/main/java/com/study/spring6_springgurucourse/domain/Book.java
+++ src/main/java/com/study/spring6_springgurucourse/domain/Book.java
@@ -4,6 +4,11 @@ package com.study.spring6_springgurucourse.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+
+import java.util.Set;
 
 @Entity
 public class Book {
@@ -13,6 +18,10 @@ public class Book {
     private long id;
     private String title;
     private String isbn;
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "book_id"))
+    private Set<Author> authors;
 
     public long getId() {
         return id;
@@ -36,5 +45,13 @@ public class Book {
 
     public void setIsbn(String isbn) {
         this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
     }
 }
