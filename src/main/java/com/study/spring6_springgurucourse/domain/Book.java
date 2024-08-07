8df9f4f7f22d6df6afde0492fb437b47b012b3c3
sofflen <nixxesgaming@gmail.com>
2024-08-07 16:36:08 +0300

    updated ManyToMany relationship

--- src/main/java/com/study/spring6_springgurucourse/domain/Book.java
+++ src/main/java/com/study/spring6_springgurucourse/domain/Book.java
@@ -23,7 +23,7 @@ public class Book {
     private String isbn;
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
-            inverseJoinColumns = @JoinColumn(name = "book_id"))
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
     public Book() {}
